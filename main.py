19e9bcfea7eee40caac0e5ba19ffbeec7da8b1ad
Don_israel <israelojakorotu@gmail.com>
2024-12-04 20:08:07 +0100

    intercept this sets

---
+++ main.py
@@ -0,0 +1,8 @@
+setx = {"green", "gold","orange","pink"}
+sety = {"blue", "gold","black","pink"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
